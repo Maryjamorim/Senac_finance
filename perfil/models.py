808ce2b8f0b0e4c981f4ed606ed3f1d7ef5b902b
Rosemary <mary_rose752@hotmail.com>
2023-08-04 20:48:39 -0300

    Finance

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50, null= False, blank=False, verbose_name='Categoria')
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0, verbose_name='valor do planejamento')
+    
+    def __str__(self):
+        return self.categoria
+    
+class Conta(models.Model):
+    banco_choices = {
+        ('NU',  'NUBANK'),
+        ('CX',  'CAIXA'),
+        ('IN',  'INTER'),
+    }
+    
+    tipo_choices = {
+        ('pf', 'Pessoa Física'),
+        ('pj', 'Pessoa Jurídica'),
+    }
+    
+    apelido = models.CharField(max_length=50)
+    banco = models.CharField(max_length=3, choices= banco_choices)
+    tipo = models.CharField(max_length=3, choices= tipo_choices)
+    valor = models.FloatField()
+    icone = models.ImageField(upload_to='Icone')
+    
+    
+    def __str__(self):
+        return self.apelido
